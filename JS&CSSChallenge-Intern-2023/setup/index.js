71037b7bee9aa3d330dd940e26c79d944c0b5fb8
DUSHAN VESHKOVIKJ <veshkovicd@yahoo.com>
2023-03-18 21:44:41 +0100

    embededsocial

---
+++ JS&CSSChallenge-Intern-2023/setup/index.js
@@ -0,0 +1,434 @@
+const data =
+    [
+        {
+            "image": "https://placekitten.com/600/400",
+            "caption": "Duis in facilisis lectus. Nulla molestie erat erat, quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2018-03-12 03:00:00",
+            "likes": "123",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/100/100"
+        },
+        {
+            "image": "https://placekitten.com/600/410",
+            "caption": "Quisque metus nisi, consequat et molestie eget, facilisis ac odio. Nam lorem libero, efficitur ac velit at, egestas tempor arcu. Nullam pharetra ex eget orci lobortis malesuada. Maecenas eget porta felis. Aliquam porttitor, nibh nec ullamcorper fermentum, eros velit lobortis justo, pretium consectetur turpis enim ut nunc. Fusce convallis, ex ut ultrices sodales, ante quam venenatis arcu, vitae mollis magna urna vel eros. Aliquam a sapien nisi. Nullam convallis malesuada suscipit. ",
+            "type": "image",
+            "source_type": "instagram",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2019-01-12 03:00:00",
+            "likes": "0",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/150/100"
+        },
+        {
+            "image": "https://placekitten.com/600/420",
+            "caption": "",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2017-01-21 03:00:00",
+            "likes": "9866555",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/100/150"
+        },
+        {
+            "image": "https://placekitten.com/600/430",
+            "caption": "Etiam eu blandit nisi. Aliquam rutrum faucibus mauris, sed egestas odio viverra at. In elementum sit amet sapien vitae bibendum.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-12 03:00:00",
+            "likes": "2",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/120/120"
+        },
+        {
+            "image": "https://placekitten.com/600/440",
+            "caption": "Vivamus a sem sit amet nisi pretium pretium. Curabitur blandit ut lectus non bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris leo odio, consectetur a lorem vitae, aliquet placerat orci. Phasellus varius ante sit amet quam mollis, eu efficitur tortor blandit.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2018-04-12 03:00:00",
+            "likes": "12343",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/50/50"
+        },
+        {
+            "image": "https://placekitten.com/600/440",
+            "caption": "Duis in facilisis lectus. Nulla molestie erat erat, quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-12 03:00:00",
+            "likes": "123",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/101/101"
+        },
+        {
+            "image": "https://placekitten.com/800/400",
+            "caption": "Duis in facilisis lectus. Nulla molestie erat erat, quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-12 03:00:00",
+            "likes": "12",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/150/120"
+        },
+        {
+            "image": "https://placekitten.com/600/500",
+            "caption": "Vivamus a sem sit amet nisi pretium pretium. Curabitur blandit ut lectus non bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris leo odio, consectetur a lorem vitae, aliquet placerat orci. Phasellus varius ante sit amet quam mollis, eu efficitur tortor blandit.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2017-12-12 03:00:00",
+            "likes": "23",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/140/140"
+        },
+        {
+            "image": "https://placekitten.com/700/400",
+            "caption": "Pellentesque lacinia volutpat turpis non fermentum. Cras at pellentesque augue. Aliquam eget metus sit amet turpis consectetur posuere.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2017-12-08 03:00:00",
+            "likes": "0",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/100/100"
+        },
+        {
+            "image": "https://placekitten.com/700/500",
+            "caption": "Duis in facilisis lectus. Nulla molestie erat erat, quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-02-12 03:00:00",
+            "likes": "0",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/102/102"
+        },
+        {
+            "image": "https://placekitten.com/800/600",
+            "caption": "Aliquam odio libero, malesuada et iaculis a, facilisis bibendum dolor. Vivamus ultricies congue arcu eu porttitor. Integer libero tortor, volutpat a purus id, mattis tristique tortor. ",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-12 03:00:00",
+            "likes": "2",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/103/100"
+        },
+        {
+            "image": "https://placekitten.com/700/600",
+            "caption": "Duis in facilisis lectus.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2020-12-12 03:00:00",
+            "likes": "1",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/40/100"
+        },
+        {
+            "image": "https://placekitten.com/1600/1400",
+            "caption": "Nulla molestie erat erat, quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-15 03:00:00",
+            "likes": "2324",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/80/100"
+        },
+        {
+            "image": "https://placekitten.com/1600/400",
+            "caption": "Aliquam odio libero, malesuada et iaculis a, facilisis bibendum dolor.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-10-12 03:00:00",
+            "likes": "1223",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/80/80"
+        },
+        {
+            "image": "https://placekitten.com/2000/4000",
+            "caption": "Quis tempor enim sodales vitae. Sed tempus, libero fringilla rhoncus ullamcorper, justo elit dignissim ex, nec elementum quam sem in urna.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2016-12-12 03:00:00",
+            "likes": "12223",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/110/110"
+        },
+        {
+            "image": "https://placekitten.com/900/600",
+            "caption": " Suspendisse finibus lorem nibh, facilisis ullamcorper nunc consequat sit amet. Curabitur ultricies magna ante, ac eleifend lorem auctor porta. Nullam volutpat aliquet lorem, et posuere ex aliquet eu. Quisque elementum sem mauris, a mattis est scelerisque vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut quis massa eget ligula euismod sagittis id congue quam. Duis vestibulum eros sed tincidunt rutrum. Pellentesque in malesuada velit.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2017-12-12 03:00:00",
+            "likes": "0",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/110/100"
+        },
+        {
+            "image": "https://placekitten.com/600/410",
+            "caption": "Quisque metus nisi, consequat et molestie eget, facilisis ac odio. Nam lorem libero, efficitur ac velit at, egestas tempor arcu. Nullam pharetra ex eget orci lobortis malesuada. Maecenas eget porta felis. Aliquam porttitor, nibh nec ullamcorper fermentum, eros velit lobortis justo, pretium consectetur turpis enim ut nunc. Fusce convallis, ex ut ultrices sodales, ante quam venenatis arcu, vitae mollis magna urna vel eros. Aliquam a sapien nisi. Nullam convallis malesuada suscipit. ",
+            "type": "image",
+            "source_type": "instagram",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2019-01-12 03:00:00",
+            "likes": "0",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/150/100"
+        },
+        {
+            "image": "https://placekitten.com/600/420",
+            "caption": "",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2017-01-21 03:00:00",
+            "likes": "9866555",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/100/150"
+        },
+        {
+            "image": "https://placekitten.com/600/430",
+            "caption": "Etiam eu blandit nisi. Aliquam rutrum faucibus mauris, sed egestas odio viverra at. In elementum sit amet sapien vitae bibendum.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://www.facebook.com/EmbedSocial/",
+            "date": "2019-12-12 03:00:00",
+            "likes": "2",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/120/120"
+        },
+        {
+            "image": "https://placekitten.com/600/440",
+            "caption": "Vivamus a sem sit amet nisi pretium pretium. Curabitur blandit ut lectus non bibendum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris leo odio, consectetur a lorem vitae, aliquet placerat orci. Phasellus varius ante sit amet quam mollis, eu efficitur tortor blandit.",
+            "type": "image",
+            "source_type": "facebook",
+            "source_link": "https://instagram.com/embedsocial/",
+            "date": "2018-04-12 03:00:00",
+            "likes": "12343",
+            "name": "John Smith",
+            "profile_image": "https://placekitten.com/g/50/50"
+        }
+    ];
+
+let cardContainer = document.querySelector(".card-container");
+
+let loadMoreBtn = document.querySelector("#load-more-btn");
+
+console.log("Data: " + data);
+
+let loadedCardsCount = 0;
+
+// this is for displaying first 4 cards
+for (let i = loadedCardsCount; i < loadedCardsCount + 4; i++) {
+    cardContainer.innerHTML += createCard(data[i]);
+}
+loadedCardsCount += 4;
+
+// add event listener to the Load More button and on this event listener four cards+ will be added
+loadMoreBtn.addEventListener("click", () => {
+    // create and display the next four cards
+    for (let i = loadedCardsCount; i < loadedCardsCount + 4; i++) {
+        if (data[i]) {
+            cardContainer.innerHTML += createCard(data[i]);
+        }
+    }
+    loadedCardsCount += 4;
+
+    // hide the Load More button when all cards have been loaded
+    if (loadedCardsCount >= data.length) {
+        loadMoreBtn.style.display = "none";
+    }
+});
+
+// add event listener to Color Input Field
+const colorInput = document.querySelector("#cardBackgroundColor");
+const cards = document.getElementsByClassName('card');
+colorInput.addEventListener("input", () => {
+    setBackgroundColor(colorInput.value);
+});
+
+function setBackgroundColor(backgroundColor) {
+    for(let i=0;i<cards.length;i++){
+        cards[i].style.backgroundColor=backgroundColor;
+    }
+}
+
+// event listener to Choose Theme input
+const lightTheme = document.getElementById('lightTheme');
+const darkTheme = document.getElementById('darkTheme');
+
+function setLightTheme() {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = 'white';
+        cards[i].style.color = 'black';
+    }
+}
+
+function setDarkTheme() {
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.backgroundColor = 'black';
+        cards[i].style.color = 'white';
+    }
+}
+
+lightTheme.addEventListener('change', function() {
+    if (lightTheme.checked) {
+        setLightTheme();
+    }
+});
+
+darkTheme.addEventListener('change', function() {
+    if (darkTheme.checked) {
+        setDarkTheme();
+    }
+});
+
+// event listener for choosing source type
+const all = document.getElementById("all");
+const instagramable = document.getElementById("instagram");
+const fb = document.getElementById("facebook");
+const twit = document.getElementById("twitter");
+
+
+instagramable.addEventListener('change', function () {
+    if (instagramable.checked) {
+        // first clear the cardContainer html
+        cardContainer.innerHTML="";
+        // second get all cards that have source type instagram
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].source_type==='instagram'){
+                cardContainer.innerHTML+=createCard(data[i]); 
+            }
+        }
+    }
+});
+
+fb.addEventListener('change', function () {
+    if (fb.checked) {
+        // first clear the cardContainer html
+        cardContainer.innerHTML="";
+        // second get all cards that have source type instagram
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].source_type==='facebook'){
+                cardContainer.innerHTML+=createCard(data[i]); 
+            }
+        }
+    }
+});
+
+twit.addEventListener('change', function () {
+    if (twit.checked) {
+        // first clear the cardContainer html
+        cardContainer.innerHTML="";
+        // second get all cards that have source type instagram
+        for (let i = 0; i < data.length; i++) {
+            if(data[i].source_type==='twitter'){
+                cardContainer.innerHTML+=createCard(data[i]);
+            }
+        }
+    }
+});
+
+all.addEventListener('change', function () {
+    if (all.checked) {
+        // first clear the cardContainer html
+        cardContainer.innerHTML="";
+        // second get all cards that have source type instagram
+        for (let i = 0; i < data.length; i++) {
+            cardContainer.innerHTML+=createCard(data[i]); 
+
+        }
+    }
+});
+
+
+
+// create a card element with the given data
+function createCard(cardData) {
+    // create the card element
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+
+
+    // add the profile image
+    const profileImage = document.createElement("img");
+    profileImage.src = cardData.profile_image;
+    profileImage.classList.add("profile-image");
+    card.appendChild(profileImage);
+
+    // add the profile name
+    const profileName = document.createElement("p");
+    profileName.textContent = cardData.name;
+    profileName.classList.add("profile-name");
+    card.appendChild(profileName);
+
+    // add the source icon
+    const sourceIcon = document.createElement("img");
+    let iconPath = '';
+    if(cardData.source_type === "facebook") {
+     
+         iconPath = cardData.source_type === "C:\Users\User\Desktop\JS&CSSChallenge-Intern-2023 (5) (2)\JS&CSSChallenge-Intern-2023\icons\facebook.svg";
+    }
+    if(cardData.source_type === "instagram") {
+      
+         iconPath = cardData.source_type === "C:\Users\User\Desktop\JS&CSSChallenge-Intern-2023 (5) (2)\JS&CSSChallenge-Intern-2023\icons\instagram-logo.svg";
+    }
+
+    sourceIcon.src = iconPath;
+    sourceIcon.classList.add("source-icon");
+    card.appendChild(sourceIcon);
+
+    // add the card image
+    const img = document.createElement("img");
+    img.src = cardData.image;
+    card.appendChild(img);
+
+    // add the card caption
+    const caption = document.createElement("p");
+    caption.textContent = cardData.caption;
+    card.appendChild(caption);
+
+    // add the card type
+    const type = document.createElement("p");
+    type.textContent = cardData.type;
+    card.appendChild(type);
+
+    // add the card source link
+    const sourceLink = document.createElement("a");
+    sourceLink.href = cardData.source_link;
+    sourceLink.textContent = cardData.source_type;
+    card.appendChild(sourceLink);
+
+    // add the card date
+    const date = document.createElement("p");
+    date.textContent = cardData.date;
+    card.appendChild(date);
+
+
+    // add the card likes counter and heart icon
+    const likes = document.createElement("div");
+    likes.classList.add("likes");
+
+    const likesCounter = document.createElement("p");
+    likesCounter.textContent = cardData.likes;
+    likes.appendChild(likesCounter);
+
+    card.appendChild(likes);
+
+
+    return card.outerHTML;
+}
+
